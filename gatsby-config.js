c85bcaf22ddbf352f881172a37d82d9db2965483
tchait0903 <115585994+tchait0903@users.noreply.github.com>
2023-03-19 00:17:27 -0400

    true fresh gatsby init

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: { icon: "src/images/icon.png" },
-    },
-  ],
-};
+  plugins: [],
+}
